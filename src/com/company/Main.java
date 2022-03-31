c8265f438f49363c0793ee528dba56020b257aad
Ajhosky <domin20050601@gmail.com>
2022-03-31 09:19:41 +0200

    zrobione postgressql

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 	// write your code here
+
+        Class.forName ("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:postgresql://msz-test.chq8qedm9jee.eu-west-1.rds.amazonaws.com/postgres",
+                "postgres",
+                "1dQxvBLTEpKTfgxQGD5FN"
+        );
+        Statement st = con.createStatement();
+        st.execute("DROP table if exists students_sobolewski");
+        st.execute("CREATE TABLE students_sobolewski(" +
+                "first_name text," +
+                "last_name text," +
+                "class_id int)");
+        st.execute("INSERT into students_sobolewski values ('Dominik', 'Sobolewski',1 )");
+        ResultSet ds = st.executeQuery("SELECT last_name, class_id FROM students_sobolewski");
+        while(ds.next()){
+            System.out.println(ds.getString("last_name"));
+            System.out.println(ds.getInt("class_id"));
+        }
+        con.close();
     }
+
 }
